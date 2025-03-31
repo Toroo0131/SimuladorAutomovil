ddffceb544dcae247ec7ea54a33aa190df0997ae
Salome Toro <salome.torod@autonoma.edu.co>
2025-03-31 16:50:04 -0500

    Se creo el repositorio y el git ignore

---
+++ src/autonoma/simmuladorautonoma/exception/Camion.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.simmuladorautonoma.exception;
+
+/**
+ *
+ * @author Salo
+ */
+public class Camion {
+    public int num;
+}
